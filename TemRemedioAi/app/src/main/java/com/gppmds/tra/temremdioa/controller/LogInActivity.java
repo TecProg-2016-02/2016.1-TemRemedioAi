b29695c7b64d2e188d98088e42e3421e91272768
Stefania Bezerra da Silva <stebezerra@hotmail.com>
2016-09-19 08:21:11 -0300

    Changing private

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/LogInActivity.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/LogInActivity.java
@@ -47,22 +47,11 @@ import com.tra.gppmds.temremdioa.R;
 
 public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    //Keep track of the login task to ensure we can cancel it if requested.
-    private UserLoginTask mAuthTask = null;
-
     CallbackManager callbackManager;
-    // res references
-    private TextView info;
-    private EditText mUsernameView;
-    private EditText mPasswordView;
-    private ProgressBar mProgressView;
-    private View mLoginFormView;
-    private View focusView = null;
-    private Button mUsernameSignInButton;
-    private Button mRegisterButton;
-    private LoginButton mFacebookButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         facebookSDKInitialize();
         setContentView(R.layout.activity_log_in);
@@ -70,9 +59,7 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         setValues();
-
         setListener();
-
     }
 
     protected void facebookSDKInitialize() {
@@ -84,12 +71,13 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
         ParseUser currentUser = ParseUser.getCurrentUser();
 
         return currentUser;
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
+
         callbackManager.onActivityResult(requestCode, resultCode, data);
         Log.e("data",data.toString());
     }
@@ -101,6 +89,7 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mUsernameSignInButton = (Button) findViewById(R.id.log_in_button);
         mRegisterButton = (Button) findViewById(R.id.log_in_sign_in_button);
+
         info = (TextView)findViewById(R.id.info);
         mFacebookButton = (LoginButton) findViewById(R.id.login_button_fb);
 
@@ -109,133 +98,14 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
 
     }
 
-    private void setListener() {
-        mFacebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-//                info.setText(
-//                        "User ID: "
-//                                + loginResult.getAccessToken().getUserId()
-//                                + "\n" +
-//                                "Auth Token: "
-//                                + loginResult.getAccessToken().getToken()
-//                );
-                finish();
-            }
-
-            @Override
-            public void onCancel() {
-                info.setText("Login attempt canceled.");
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
-            }
-        });
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        mUsernameSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-
-            }
-        });
-
-        mRegisterButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signUpActivity = new Intent(LogInActivity.this, SignUpActivity.class);
-                startActivity(signUpActivity);
-            }
-        });
-    }
-
-    // Attempts to login in the system if the entries email and password are valid
-    private void attemptLogin(){
-        // Store values at the time of the login attempt
-        final String username = mUsernameView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        if (mAuthTask != null){
-            return;
-        }
-
-        if ((validateError(username, "Ops! Campo de username esta vazio.", mUsernameView) ||
-                validateError(password, "Ops! Campo do password esta vazio.", mPasswordView))) {
-
-            focusView.requestFocus();
-
-        } else {
-            showProgress(true);
-            ParseUser.logInInBackground(username, password, new LogInCallback() {
-                @Override
-                public void done(ParseUser parseUser, ParseException e) {
-                    if (parseUser != null) {
-
-                        Toast.makeText(getApplicationContext(), "Logado com sucesso, seja bem vindo " + username +"!", Toast.LENGTH_LONG).show();
-                        finish();
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Nome de usuário e/ou senha não existente!", Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(LogInActivity.this, LogInActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-            });
-        }
-    }
-
     public boolean validateError(String word, String phrase, EditText text) {
+
         String phraseValited = TextUtils.isEmpty(word) ? phrase : null;
         text.setError(phraseValited);
+
         focusView = phraseValited != null ? text : null;
+
         return TextUtils.isEmpty(word);
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) { // Honeycombe api use to show progress bar
-
-        mProgressView.getIndeterminateDrawable().setColorFilter(Color.RED,
-                                                            android.graphics.PorterDuff.Mode.SRC_IN);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
     }
 
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
@@ -255,17 +125,9 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
             try {
                 // Simulate network access.
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException exception) {
                 return false;
             }
-
-//            for (String credential : DUMMY_CREDENTIALS) {
-//                String[] pieces = credential.split(":");
-//                if (pieces[0].equals(mEmail)) {
-//                    // Account exists, return true if the password matches.
-//                    return pieces[1].equals(mPassword);
-//                }
-//            }
 
             // TODO: register the new account here.
             return true;
@@ -293,6 +155,7 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
         return new CursorLoader(this,
                 // Retrieve data rows for the device user's 'profile' contact.
                 Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
@@ -303,12 +166,170 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
                         " = ?", new String[]{ContactsContract.CommonDataKinds.Email
                 .CONTENT_ITEM_TYPE},
 
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
+                /* Show primary email addresses first. Note that there won't be
+                    a primary email address if the user hasn't specified one. */
                 ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
     }
 
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+        finish();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+        finish();
+    }
+    // res references
+    private TextView info;
+
+    private EditText mUsernameView;
+
+    private EditText mPasswordView;
+
+    private ProgressBar mProgressView;
+
+    private View mLoginFormView;
+
+    private View focusView = null;
+
+    private Button mUsernameSignInButton;
+
+    private Button mRegisterButton;
+
+    private LoginButton mFacebookButton;
+
+    //Keep track of the login task to ensure we can cancel it if requested.
+    private UserLoginTask mAuthTask = null;
+
+    private void setListener() {
+        mFacebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+                info.setText("Login attempt failed.");
+            }
+        });
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mUsernameSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                attemptLogin();
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpActivity = new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(signUpActivity);
+            }
+        });
+    }
+
+    // Attempts to login in the system if the entries email and password are valid
+    private void attemptLogin(){
+
+        // Store values at the time of the login attempt
+        final String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (mAuthTask != null){
+            return;
+        }
+
+        if ((validateError(username, "Ops! Campo de username esta vazio.", mUsernameView) ||
+                validateError(password, "Ops! Campo do password esta vazio.", mPasswordView))) {
+
+            focusView.requestFocus();
+        } else {
+            showProgress(true);
+            ParseUser.logInInBackground(username, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser parseUser, ParseException e) {
+
+                    if (parseUser != null) {
+
+                        Toast.makeText(getApplicationContext(), "Logado com sucesso, seja bem vindo " + username +"!",
+                                Toast.LENGTH_LONG).show();
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Nome de usuário e/ou senha não existente!",
+                                Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(LogInActivity.this, LogInActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            });
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // Honeycombe api use to show progress bar
+
+        mProgressView.getIndeterminateDrawable().setColorFilter(Color.RED,
+                android.graphics.PorterDuff.Mode.SRC_IN);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            /* The ViewPropertyAnimator APIs are not available, so simply show
+               and hide the relevant UI components.*/
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
     private interface ProfileQuery {
+
         String[] PROJECTION = {
                 ContactsContract.CommonDataKinds.Email.ADDRESS,
                 ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
@@ -316,15 +337,5 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
 
         int ADDRESS = 0;
         int IS_PRIMARY = 1;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        finish();
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        finish();
     }
 }
